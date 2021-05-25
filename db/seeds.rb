3629f9e1e3c314a02a9c6478d3e822523d5381fd
Eduard Ciugulea <ciugulea@yahoo.com>
2021-05-25 23:03:14 +0100

    Working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  puts 'start to create'
+  Restaurant.create(
+    name: ['Burger palce', 'Pizza place', 'Some place to eat', 'Gym'].sample,
+    address: 'london',
+    phone_number: '07CallMeLater',
+    category: Restaurant::CATEGORY.sample
+  )
+  puts 'I create '
+end
+puts 'done'
